e9a880636dced39991c4b3e7a0dc9737d6d4abe2
Roberto Quispe Tangherlini <admin@topsevenoficial.com>
2025-02-14 22:06:13 -0500

    M4

--- config/server.ts
+++ config/server.ts
@@ -1,14 +1,7 @@
-// config/server.ts
-module.exports = ({ env }) => {
-  // Si process.env.PORT existe (como en Render), se usa; de lo contrario, se utiliza la variable PORT definida en env o 1337 por defecto.
-  const port = process.env.PORT
-    ? parseInt(process.env.PORT, 10)
-    : env.int("PORT", 1337);
-  return {
-    host: env("HOST", "0.0.0.0"),
-    port,
-    app: {
-      keys: env.array("APP_KEYS"),
-    },
-  };
-};
+export default ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+});
