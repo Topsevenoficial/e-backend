7a04bbfce8a1a448f57a5503880c55f75dca040f
Roberto Quispe Tangherlini <admin@topsevenoficial.com>
2025-02-15 13:44:33 -0500

    M10

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,14 +1,12 @@
-import { StrapiConfig } from 'strapi'; // Si cuentas con tipos instalados, sino omite esta línea
-
 export default ({ env }: { env: (key: string) => string }) => ({
   // Configuración del plugin de Cloudinary para Upload
   upload: {
     config: {
-      provider: 'cloudinary',
+      provider: "cloudinary",
       providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
       },
       actionOptions: {
         upload: {},
@@ -17,15 +15,15 @@ export default ({ env }: { env: (key: string) => string }) => ({
       },
     },
   },
-  // Configuración de otro plugin (ejemplo: Neon Tech DB Branches)
-  'strapi-neon-tech-db-branches': {
+  // Configuración de otro plugin
+  "strapi-neon-tech-db-branches": {
     enabled: true,
     config: {
       neonApiKey:
-        'napi_86pjglpao6qrdiw1083pu6qpwj4ev63apv9vw04jknz34ys3tos9d0rqkczy5aao', // Clave de Neon Tech
-      neonProjectName: 'Ecommerce', // Nombre del proyecto en Neon
-      neonRole: 'neondb_owner', // Rol configurado en tu proyecto
-      gitBranch: 'main', // Rama de Git (opcional)
+        "napi_86pjglpao6qrdiw1083pu6qpwj4ev63apv9vw04jknz34ys3tos9d0rqkczy5aao", // Clave de Neon Tech
+      neonProjectName: "Ecommerce", // Nombre del proyecto en Neon
+      neonRole: "neondb_owner", // Rol configurado en tu proyecto
+      gitBranch: "main", // Rama de Git (opcional)
     },
   },
   // Otras configuraciones de plugins pueden ir aquí...
