90d88016a7512dae8816f9ad40d2ca6f7a4a3594
Roberto Quispe Tangherlini <admin@topsevenoficial.com>
2025-02-14 22:41:52 -0500

    M6

--- config/server.ts
+++ config/server.ts
@@ -1,12 +1,7 @@
-// config/server.ts
-export default ({ env }) => {
-  // Si process.env.PORT está definido, úsalo; si no, usa el valor de env('PORT') o 1337
-  const port = process.env.PORT ? parseInt(process.env.PORT, 10) : env.int('PORT', 1337);
-  return {
-    host: env('HOST', '0.0.0.0'),
-    port,
-    app: {
-      keys: env.array('APP_KEYS'),
-    },
-  };
-};
+export default ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+});
